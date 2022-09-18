0c8298785f12ede5b9a4fa28b070683d236047f1
Mohammed Zidan M P <zidanmp786@gmail.com>
2022-09-18 16:22:31 +0530

    Create Transaction.java

---
+++ src/Transaction.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class Transaction {
+    private double amount;
+    private Date timestamp;
+    private String memo;
+    private Account inAccount;
+    
+    public Transaction(double amount , Account inAccount) {
+        this.amount = amount ;
+        this.inAccount = inAccount ;
+        this.timestamp = new Date();
+        this.memo = "";
+    }
+    
+    public Transaction(double amount, String memo, Account inAccount) {
+		this(amount, inAccount);
+		this.memo = memo;	
+	}
+    
+    public double getMoney() {
+        return this.amount;
+    }
+    
+    public void transactionInfo() {
+        System.out.println(this.amount + " " +this.inAccount +  " " + this.memo + " "+ this.timestamp);
+    }   
+}
